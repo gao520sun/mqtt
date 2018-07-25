84b539f4cc8c057b4abef50cd2dec96406cf1171
gaozhonglei <123456>
2018-07-25 18:13:13 +0800

    first commit

---
+++ ios/RCTMqtt/RCTMqtt.h
@@ -0,0 +1,13 @@
+//
+//  RCTMqtt.h
+//  RCTMqtt
+//
+//  Created by gaozhonglei on 2018/7/25.
+//  Copyright © 2018年 gaozhonglei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTMqtt : NSObject
+
+@end
---
+++ ios/RCTMqtt/RCTMqtt.m
@@ -0,0 +1,13 @@
+//
+//  RCTMqtt.m
+//  RCTMqtt
+//
+//  Created by gaozhonglei on 2018/7/25.
+//  Copyright © 2018年 gaozhonglei. All rights reserved.
+//
+
+#import "RCTMqtt.h"
+
+@implementation RCTMqtt
+
+@end
